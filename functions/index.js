2f86e4801fb45399abe1628c4c8eec664feadb5d
poveride <poveride@gmail.com>
2022-04-29 23:58:36 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,8 @@ const functions = require("firebase-functions");
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.storeCheckout = functions.https.onRequest((request, response) => {
-  const totalCost = request.body.data.reduce((total, item) => {
-    return total += item.price;
+  const totalCost = request.body.data.reduce((total, iteem) => {
+    return total += iteem.price;
   }, 0);
   // functions.logger.info("Hello logs!", {structuredData: true});
   response.send({
